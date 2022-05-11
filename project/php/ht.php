942e1dbadb74cbafd2d16970c295d69371be9ba0
amirbaseet <103933162+amirbaseet@users.noreply.github.com>
2022-05-11 14:51:26 +0300

    commit 10

---
+++ project/php/ht.php
@@ -0,0 +1,14 @@
+<?php
+echo "bilgiler başarıyla gönderildi\n";
+$name=$_POST['flname'];
+$email=$_POST['email'];
+$msg=$_POST['message'];
+echo "<br>";
+
+echo "isim:"."&nbsp;".$name."\n";
+echo "<br>";
+echo "email:"."&nbsp;".$email."\n";
+echo "<br>";
+
+echo "message:"."&nbsp;".$msg."\n";
+?>
